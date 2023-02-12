8aea414cc5817d89aab69ddabf8fcf6dbce9f10c
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-12 14:00:40 +0600

    Добавил модель Article в модели

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False, verbose_name="Заголовок")
+    text = models.TextField(max_length=3000, null=False, verbose_name="Текст")
+    author = models.CharField(max_length=200, null=False, verbose_name="Автор")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время обновления")
+
+    def __str__(self):
+        return f"{self.author} - {self.title}"
