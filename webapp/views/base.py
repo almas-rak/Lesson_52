60929d29367cece6f9953d7f073a64ecbedad442
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-02-12 20:29:39 +0600

    удалили калькулятор в контекст передали список объектов articles

--- webapp/views/base.py
+++ webapp/views/base.py
@@ -1,38 +1,12 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
+from webapp.models import Article
+
 
 def index_view(request: WSGIRequest):
-    first = int(request.GET.get('first', 0))
-    second = int(request.GET.get('second', 0))
-    method = (request.GET.get('method', ''))
-    print(first)
-    print(second)
-    print(method)
-    match method:
-        case '-':
-            result = first - second
-        case '+':
-            result = first + second
-        case '*':
-            result = first * second
-        case _:
-            result = 0
-    # print(f"id {request.GET.get('id')}")
-    # print(f'name {request.GET.get("name")}')
-    print(f"METHOD:  {request.method}")
-    # print(f"Get id - {request.GET.get('id')} - {type(request.GET.get('id'))}")
-    print(request)
-    print(request.GET)
-    return render(request, 'index.html', context={
-        'result': result,
-        'first': first,
-        'second': second,
-        'method': method
-    })
-
-# context={
-#         'id': request.GET.get('id', 'no id'),
-#         'name': request.GET.get("name", 'no name')
-#
-#     }
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'index.html', context=context)
